bc6931b696689f28a0d1c90a0a4fae536f5e7f1b
HaLamUs <itzlamz9x@gmail.com>
2024-08-08 01:21:41 +0700

    feat!: init project

---
+++ weather/weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by lamha on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
