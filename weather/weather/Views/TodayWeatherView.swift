0af6a1e1703b45d0b4d2413f8b6d3f961eacb05a
HaLamUs <itzlamz9x@gmail.com>
2024-08-08 23:27:27 +0700

    chore: clean code

--- weather/weather/Views/TodayWeatherView.swift
+++ weather/weather/Views/TodayWeatherView.swift
@@ -16,7 +16,7 @@ struct TodayWeatherView: View {
                 .font(.largeTitle)
                 .bold()
             
-            VStack(spacing: 20) {
+            HStack(spacing: 20) {
                 LottieView(name: cityViewModel.getLottieAnimationFor(icon: cityViewModel.weatherIcon))
                     .frame(width: 100, height: 100)
                 VStack(alignment: .leading) {
@@ -26,53 +26,6 @@ struct TodayWeatherView: View {
                 }
             }
             
-//            List{
-//                VStack(spacing: 20) {
-//                    LottieView(name: "dayClearSky")
-//                        .frame(width: 100, height: 100)
-//                    LottieView(name: "nightClearSky")
-//                        .frame(width: 100, height: 100)
-//                    LottieView(name: "dayFewClouds")
-//                        .frame(width: 100, height: 100)
-//                    LottieView(name: "nightFewClouds")
-//                        .frame(width: 100, height: 100)
-//                    LottieView(name: "dayScatteredClouds")
-//                        .frame(width: 100, height: 100)
-//                    LottieView(name: "nightScatteredClouds")
-//                        .frame(width: 100, height: 100)
-//                    LottieView(name: "dayBrokenClouds")
-//                        .frame(width: 100, height: 100)
-//                    LottieView(name: "nightBrokenClouds")
-//                        .frame(width: 100, height: 100)
-//
-//                    LottieView(name: "dayShowerRains")
-//                        .frame(width: 100, height: 100)
-//                    LottieView(name: "nightShowerRains")
-//                        .frame(width: 100, height: 100)
-//
-//                    LottieView(name: "dayRain")
-//                        .frame(width: 100, height: 100)
-//                    LottieView(name: "nightRain")
-//                        .frame(width: 100, height: 100)
-//                    LottieView(name: "dayThunderstorm")
-//                        .frame(width: 100, height: 100)
-//                    LottieView(name: "nightThunderstorm")
-//                        .frame(width: 100, height: 100)
-//                    LottieView(name: "daySnow")
-//                        .frame(width: 100, height: 100)
-//                    LottieView(name: "nightSnow")
-//                        .frame(width: 100, height: 100)
-//                    LottieView(name: "dayClearSky")
-//                        .frame(width: 100, height: 100)
-//                    LottieView(name: "nightShowerRains")
-//                        .frame(width: 100, height: 100)
-//                }
-////                VStack(alignment: .leading) {
-////                    Text("\(cityViewModel.temperature)℃")
-////                        .font(.system(size: 42))
-////                    Text(cityViewModel.conditions)
-////                }
-//            }
             HStack {
                 Spacer()
                 widgetView(image: "wind", color: .green, title: "\(cityViewModel.windSpeed) km/hr")
@@ -88,9 +41,9 @@ struct TodayWeatherView: View {
         .background(
             RoundedRectangle(cornerRadius: 20)
                 .fill(LinearGradient(
-                        gradient: Gradient(colors: [Color.blue.opacity(0.5), Color.blue]),
-                        startPoint: .top,
-                        endPoint: .bottom))
+                    gradient: Gradient(colors: [Color.blue.opacity(0.5), Color.blue]),
+                    startPoint: .top,
+                    endPoint: .bottom))
                 .opacity(0.3)
         )
         .shadow(color: Color.white.opacity(0.1), radius: 2, x: -2, y: -2)
