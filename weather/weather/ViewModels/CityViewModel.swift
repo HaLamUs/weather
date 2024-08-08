228276ab57958ef5c3ea1aac49708bd191d38394
HaLamUs <itzlamz9x@gmail.com>
2024-08-08 23:05:07 +0700

    feat: add view model

---
+++ weather/weather/ViewModels/CityViewModel.swift
@@ -0,0 +1,209 @@
+//
+//  CityViewModel.swift
+//  weather
+//
+//  Created by lamha on 08/08/2024.
+//
+
+import Foundation
+import CoreLocation
+
+final class CityViewModel: ObservableObject {
+    @Published var weather = WeatherResponse.empty()
+    @Published var city: String = "Ho chi minh" {
+        didSet {
+            getLocation()
+        }
+    }
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .full
+        return formatter
+    }()
+    
+    private lazy var dayFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEE"
+        return formatter
+    }()
+    
+    private lazy var timeFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh a"
+        return formatter
+    }()
+    
+    init() {
+        getLocation()
+    }
+    
+    var date: String {
+        return dateFormatter.string(
+            from: Date(timeIntervalSince1970: TimeInterval(weather.current.dt))
+        )
+    }
+    
+    var weatherIcon: String {
+        if weather.current.weather.count > 0 {
+            return weather.current.weather[0].icon
+        }
+        return "dayClearSky"
+    }
+    
+    var temperature: String {
+        getTempFor(temp: weather.current.temp)
+    }
+    
+    var conditions: String {
+        if weather.current.weather.count > 0 {
+            return weather.current.weather[0].main
+        }
+        return ""
+    }
+    
+    var windSpeed: String {
+        String(format: "%0.1f", weather.current.wind_speed)
+    }
+    
+    var humidity: String {
+        String(format: "%d%%", weather.current.humidity)
+    }
+    
+    var rainChances: String {
+        String(format: "%0.1f%%", weather.current.dew_point)
+    }
+    
+    func getTimeFor(timestamp: Int) -> String {
+        return timeFormatter.string(
+            from: Date(timeIntervalSince1970: TimeInterval(timestamp))
+        )
+    }
+    
+    func getTempFor(temp: Double) -> String {
+        String(format: "%0.1f", temp)
+    }
+    
+    func getDayFor(timestamp: Int) -> String {
+        dayFormatter.string(
+            from: Date(timeIntervalSince1970: TimeInterval(timestamp))
+        )
+    }
+    
+    private func getLocation() {
+        CLGeocoder().geocodeAddressString(city) { (placemarks, error) in
+            if let places = placemarks, let place = places.first {
+                self.getWeather(coord: place.location?.coordinate)
+            }
+        }
+    }
+    
+    private func getWeather(coord: CLLocationCoordinate2D?) {
+        if let coord = coord {
+            let urlString = Api.getUrlFor(lat: coord.latitude, lon: coord.longitude)
+            getWeatherInternal(city: city, for: urlString)
+        } else {
+            let urlString = Api.getUrlFor(lat: 0, lon: 0)
+            getWeatherInternal(city: city, for: urlString)
+        }
+    }
+    
+    private func getWeatherInternal(city: String, for urlString: String) {
+        NetworkManager<WeatherResponse>.fetch(for: URL(string: urlString)!) { (result) in
+            switch result {
+            case .success(let response):
+                DispatchQueue.main.async {
+                    self.weather = response
+                }
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
+    
+    func getLottieAnimationFor(icon: String) -> String {
+        switch icon {
+        case "01d":
+            return "dayClearSky"
+        case "01n":
+            return "nightClearSky"
+        case "02d":
+            return "dayFewClouds"
+        case "02n":
+            return "nightFewClouds"
+        case "03d":
+            return "dayScatteredClouds"
+        case "03n":
+            return "nightScatteredClouds"
+        case "04d":
+            return "dayBrokenClouds"
+        case "04n":
+            return "nightBrokenClouds"
+        case "09d":
+            return "dayShowerRains"
+        case "09n":
+            return "nightShowerRains"
+        case "10d":
+            return "dayRain"
+        case "10n":
+            return "nightRain"
+        case "11d":
+            return "dayThunderstorm"
+        case "11n":
+            return "nightThunderstorm"
+        case "13d":
+            return "daySnow"
+        case "13n":
+            return "nightSnow"
+        case "50d":
+            return "dayClearSky"
+        case "50n":
+            return "dayClearSky"
+        default:
+            return "dayClearSky"
+        }
+    }
+    
+    func getWeatherIconFor(icon: String) -> String {
+        switch icon {
+        case "01d":
+            return "sun.max.fill"
+        case "01n":
+            return "moon.fill"
+        case "02d":
+            return "cloud.sun.fill"
+        case "02n":
+            return "cloud.moon.fill"
+        case "03d":
+            return "cloud.fill"
+        case "03n":
+            return "cloud.fill"
+        case "04d":
+            return "cloud.fill"
+        case "04n":
+            return "cloud.fill"
+        case "09d":
+            return "cloud.drizzle.fill"
+        case "09n":
+            return "cloud.drizzle.fill"
+        case "10d":
+            return "cloud.heavyrain.fill"
+        case "10n":
+            return "cloud.heavyrain.fill"
+        case "11d":
+            return "cloud.bolt.fill"
+        case "11n":
+            return "cloud.bolt.fill"
+        case "13d":
+            return "cloud.snow.fill"
+        case "13n":
+            return "cloud.snow.fill"
+        case "50d":
+            return "cloud.fog.fill"
+        case "50n":
+            return "cloud.fog.fill"
+        default:
+            return "sun.max.fill"
+        }
+    }
+}
