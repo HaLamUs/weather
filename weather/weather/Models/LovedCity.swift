2cf9b0676dd539e0373e3ed8aa6757bfa70e9e8e
HaLamUs <itzlamz9x@gmail.com>
2024-08-13 01:19:08 +0700

    chore: update city model with latest syntax

--- weather/weather/Models/LovedCity.swift
+++ weather/weather/Models/LovedCity.swift
@@ -9,10 +9,6 @@ import Foundation
 import RealmSwift
 
 class LovedCity: Object {
-    @objc dynamic var id: Int64 = -1
-    @objc dynamic var name: String = ""
-    
-    override static func primaryKey() -> String? {
-        return "id"
-    }
+    @Persisted(primaryKey: true) var id: Int = -1
+    @Persisted var name: String = ""
 }
