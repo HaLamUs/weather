3d3144a8844ea8600e5dbcc5d6b0b4ec775cabc5
HaLamUs <itzlamz9x@gmail.com>
2024-08-12 19:29:34 +0700

    feat: add realm manager

---
+++ weather/weather/Repositories/DatabaseInterface.swift
@@ -0,0 +1,17 @@
+//
+//  DatabaseInterface.swift
+//  weather
+//
+//  Created by lamha on 12/08/2024.
+//
+
+import Foundation
+
+protocol DatabaseManager {
+    associatedtype Entity
+
+    func create(_ object: Entity)
+    func read(with primaryKey: Any) -> Entity?
+    func update(_ object: Entity, with dictionary: [String: Any])
+    func delete(_ object: Entity)
+}
